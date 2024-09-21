4970ad04b66b87e7dbd0b012bdfe7e3b6935a184
Fieeeshie <165393567+Fieeeshie@users.noreply.github.com>
2024-09-22 01:02:58 +0800

    #37emojiconverter

---
+++ src/app/components/emojiconverter.component/emojiconverter.component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emojiconverter.component',
+  standalone: true,
+  imports: [],
+  templateUrl: './emojiconverter.component.component.html',
+  styleUrl: './emojiconverter.component.component.css'
+})
+export class EmojiconverterComponentComponent {
+
+}

ba617821f5dc2741f9b72a0cd3efcbac76a43cb5
Fieeeshie <165393567+Fieeeshie@users.noreply.github.com>
2024-09-23 14:19:40 +0800

    #appcomponent-and-appmodule-etc

--- src/app/components/userage/userage.component.ts
+++ src/app/components/userage/userage.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './userage.component.css'
 })
 export class UserageComponent {
+  birthYear: number | null = null;
+  age: number | null = null;
 
+  calculateAge() {
+    if (this.birthYear) {
+      const currentYear = new Date().getFullYear();
+      this.age = currentYear - this.birthYear;
+    }
+  }
 }
